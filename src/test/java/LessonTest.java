2ca1931ed6437c8bba5c61a81463796341c93fd6
romina <rominamirfattahi13@gmail.com>
2025-08-17 23:58:26 +0330

    first commit

---
+++ src/test/java/LessonTest.java
@@ -0,0 +1,21 @@
+import mftplus.model.entity.Lesson;
+import mftplus.model.service.LessonService;
+
+import java.time.LocalDate;
+
+public class LessonTest {
+    public static void main(String[] args) throws Exception {
+        Lesson lesson = Lesson
+                .builder()
+                .personId(1)
+                .title("math")
+                .code(123)
+                .teacher("mrs.azizi")
+                .unit("first")
+                .startDataTime(LocalDate.of(2022,1,3))
+                .build();
+
+
+        LessonService.getService().save(lesson);
+    }
+}
