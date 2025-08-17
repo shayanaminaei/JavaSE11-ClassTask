4aa5f1faedec54f9bb0b9c8fcd1e93d4c5a50567
Shayan Aminaei <shayanaminaei@gmail.com>
2025-08-17 20:05:28 +0330

    Comit

--- src/main/java/mftplus/model/repository/CarRepository.java
+++ src/main/java/mftplus/model/repository/CarRepository.java
@@ -6,17 +6,51 @@ import mftplus.model.tools.PersonMapper;
 import org.jetbrains.annotations.NotNull;
 
 import java.sql.*;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
 
 public class CarRepository implements Repository<Car, Integer>, AutoCloseable {
-    private final Connection connection;
+    //private final Connection connection;
     private PreparedStatement preparedStatement;
     private final PersonMapper personMapper = new PersonMapper();
 
     public CarRepository(Connection connection) throws SQLException {
         //connection.ConnectionProvider.getProvider().getConnection();
+
     }
+
+    @Override
+    public void close() throws Exception {
+
+    }
+
+    @Override
+    public void save(Car car) throws Exception {
+
+    }
+
+    @Override
+    public void edit(Car car) throws Exception {
+
+    }
+
+    @Override
+    public void delete(Integer id) throws Exception {
+
+    }
+
+    @Override
+    public List<Car> findAll() throws Exception {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public Car findById(Integer id) throws Exception {
+        return null;
+    }
+}
 
 
 
