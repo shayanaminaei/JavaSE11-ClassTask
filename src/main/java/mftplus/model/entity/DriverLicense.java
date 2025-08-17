724da0f2b88b8d852aab59144e989b12cdf77c42
Zahra <moradi.nehad.137@gmail.com>
2025-08-17 20:35:21 +0330

    driver license updated

--- src/main/java/mftplus/model/entity/DriverLicense.java
+++ src/main/java/mftplus/model/entity/DriverLicense.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 @Getter
 @Setter
 
-public class DriverLicenses {
+public class DriverLicense {
     private int id;
     private String personId;
     private String serial;
