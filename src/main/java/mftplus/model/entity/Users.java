de1c4f319b5f6b297c6e24e0db2b501bb4cd35c8
Hedie-khodaei <hedi2002saniyani@gmail.com>
2025-08-31 19:45:49 +0330

    aa

---
+++ src/main/java/mftplus/model/entity/Users.java
@@ -0,0 +1,32 @@
+package mftplus.model.entity;
+
+import com.google.gson.Gson;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
+
+public class Users {
+    private int id;
+    private Person person;
+    private String name;
+    private String family;
+    private  String username;
+    private String password;
+    private String nickname;
+    private Boolean locked;
+    private LocalDate registerDate;
+
+
+    @Override
+    public String toString() {
+        Gson gson = new Gson();
+        return gson.toJson(this);
+    }
+}
