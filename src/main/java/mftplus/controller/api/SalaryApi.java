b43d51f17ba5bb5f85dab91595dc48b32a476cb8
amir <amirsobhanghandi@gmail.com>
2025-09-02 17:21:30 +0330

    Salaries section

---
+++ src/main/java/mftplus/controller/api/SalaryApi.java
@@ -0,0 +1,45 @@
+package mftplus.controller.api;
+
+
+import jdk.jpackage.internal.Log;
+import lombok.extern.log4j.Log4j;
+import mftplus.model.entity.Salary;
+import mftplus.model.service.SalaryService;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+@Log4j
+@Path("/Salary")
+public class SalaryApi {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getSalary(@QueryParam("token") String token) {
+        if  (token != null && token.equals("abc123")) {
+            return Response.ok().build();
+        }
+        return Response.status(Response.Status.FORBIDDEN).build();
+    }
+    @POST
+    public Response save(
+            @QueryParam("personId") int personId,
+            @QueryParam("weeklyHour") int weeklyHour,
+            @QueryParam("payPerHour") int payPerHour,
+            @QueryParam("startDate") String startDate,
+            @QueryParam("endDate") String endDate,
+            @QueryParam("employeeType") String employeeType,
+            @QueryParam("token") String token
+    ) {
+        try {
+            Salary salary = new Salary();
+            SalaryService.getService().save(salary);
+            log.info("Salary saved successfully");
+            return Response.ok(salary).build();
+        } catch (Exception e) {
+            Log.error(e.getMessage());
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+        }
+
+
+    }
+}
