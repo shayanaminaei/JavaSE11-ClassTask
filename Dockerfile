e6fc124913ef0c2fc630b1de4d0a04fe62771a6b
Messbah.A <Messbah.A@Gmail.com>
2025-09-07 20:27:27 +0330

    debugged

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM ubuntu:latest
+LABEL authors="Student"
+
+ENTRYPOINT ["top", "-b"]
--- src/main/java/mftplus/controller/AccountsTest.java
+++ src/main/java/mftplus/controller/AccountsTest.java
@@ -1,9 +1,11 @@
 package mftplus.controller;
 
-import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
 
-@Log4j
+@Log4j2
 public class AccountsTest {
     public static void main(String[] args) throws Exception {
         log.info("Starting Accounts Test");
         log.error("Error saving");
+    }
+}
